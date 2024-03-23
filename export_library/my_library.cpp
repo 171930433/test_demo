be402e3ebfc3b9e4c7821e5d899ed7d682bd8467
gsk <171930433@qq.com>
2024-03-23 22:19:31 +0800

    添加了库导出的 MyLibraryConfig.cmake MyLibraryTargets.cmake

---
+++ export_library/my_library.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "my_library.h"
+
+void hello() {
+    std::cout << "Hello, from MyLibrary!" << std::endl;
+}
---
+++ use_expot_library/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "my_library.h"
+
+int main() {
+    // 调用 MyLibrary 提供的函数
+    hello();
+
+    return 0;
+}
